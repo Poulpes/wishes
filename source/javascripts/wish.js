eb88870452e89bf79e397a4c14fb72a0779fd875
Karine Bialobroda <karine.bialobroda@gmail.com>
2018-01-15 17:40:26 +0100

    remove console log

--- source/javascripts/wish.js
+++ source/javascripts/wish.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     var position = $('.bg-red').offset().top;
     var half = $('.section').height() / 2;
     if ($(window).scrollTop() > position - half ) {
-      console.log('yo');
       $('#new-year').addClass('animate');
     }
   });
